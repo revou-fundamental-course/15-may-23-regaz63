84d07942e3b34775f72ca824c2d040f45870cc0f
unknown <onehokage0@gmail.com>
2023-05-25 19:27:48 +0800

    add Js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 function hitungluas() { 
-    var sisiluas = document.getElementById("sisi-luas").value;
 
-    var style = document.getElementsByClassName("result-luas");
-    console.log(style);
-    document.getElementById("output-luas").innerHTML = '<span>'+sisiluas+'</span>';
+    var sisiluas = document.getElementById("sisi-luas").value;
+    sisiluas = parseInt(sisiluas);
+    var hasil = sisiluas * sisiluas;
+    document.getElementById("output-luas").innerHTML = hasil;
+    document.getElementById("perhitungan-luas").innerHTML = sisiluas;
+    document.getElementById("perhitungan-luas2").innerHTML = sisiluas;
 }
