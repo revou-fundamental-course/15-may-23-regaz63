0b874631a3e15992052db82bd59f613b2b7f98ce
unknown <onehokage0@gmail.com>
2023-05-25 20:08:38 +0800

    push

--- js/script.js
+++ js/script.js
@@ -1,9 +1,4 @@
 function hitungluas() { 
 
-    var sisiluas = document.getElementById("sisi-luas").value;
-    sisiluas = parseInt(sisiluas);
-    var hasil = sisiluas * sisiluas;
-    document.getElementById("output-luas").innerHTML = hasil;
-    document.getElementById("perhitungan-luas").innerHTML = sisiluas;
-    document.getElementById("perhitungan-luas2").innerHTML = sisiluas;
+    
 }
